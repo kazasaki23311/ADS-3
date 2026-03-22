038c31bffa44de8fd0173523ee9500429bb1b47b
kazasaki23311 <julia.nikashina@gmail.com>
2026-03-22 18:59:49 +0300

    nikashina.cpp
    1-2 nikashina

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,38 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+  uint64_t maxValue = 1;
+  while (num != 1) {
+    if (num > maxValue) {
+      maxValue = num;
+    }
+    if (num % 2 == 0) {
+      num /= 2;
+    }
+    else {
+      num = num * 3 + 1;
+    }
+  }
+  return maxValue;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  if (num == 1) return 1;
+  else if (num % 2 == 1) return 1 + collatzLen(3 * num + 1);
+  else if (num % 2 == 0) return 1 + collatzLen(num / 2);
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+  unsigned int num = 0;
+  *maxlen = 0;
+  for (lbound; lbound <= rbound; lbound++) {
+    uint64_t tlen = collatzLen(lbound);
+    if (tlen > *maxlen) {
+      *maxlen = tlen;
+      num = lbound;
+    }
+  }
+  return num;
 }
